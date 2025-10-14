3ed67d367a7e3a44f92773908601ac18c5a414bf
Oriol <63050134+Oriol-Ferret@users.noreply.github.com>
2025-10-14 16:59:47 +0200

    Update base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/SociGest/' : '/',
   plugins: [react(), tailwindcss()],
+  base: "/SociGest",
 });
